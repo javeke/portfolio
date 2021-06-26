42a3186c21f8439abc80f535a4b6d8981fd36ccb
Javier <javierbryan11@gmail.com>
2021-06-26 01:11:40 -0500

    added logo spin animation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="/">
-                <img src="../assets/logo.png" alt="personal-logo" width="24" height="24" class="d-inline-block align-text-top profile-pic">
+                <img src="../assets/logo.png" alt="personal-logo" class="d-inline-block align-text-top profile-pic">
                 Javier Bryan
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,7 +22,18 @@
 <style scoped>
 
 .profile-pic{
-    border-radius: 50%;
+    width: 25px;
+    height: 25px;
+    animation: spin 5s infinite linear;
+}
+
+@keyframes spin {
+    from{
+        transform: rotate(0);
+    }
+    to{
+        transform: rotate(360deg);
+    }
 }
 
 </style>
