b1c44172ab1fbdeaffa1a9e0f2fef8220e015a4f
Javier <javierbryan11@gmail.com>
2022-03-05 23:23:57 -0500

    updated resume

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,7 +15,7 @@
                     <a class="nav-link" :class="{'active':activeLink=='#about'}" href="#about">About</a>
                     <a class="nav-link" :class="{'active':activeLink=='#contact'}" href="#contact">Contact</a>
                     <a class="nav-link" :class="{'active':activeLink=='#portfolio'}" href="#portfolio">Portfolio</a>
-                    <a class="nav-link" href="./Javier Bryan- Resume 2021.pdf" target="_blank">Resume</a>
+                    <a class="nav-link" href="./Javier Bryan- Resume 2022.pdf" target="_blank">Resume</a>
                 </div>
             </div>
         </nav>
@@ -28,7 +28,7 @@
                 <a class="nav-link" @click.stop="closeSidebar" :class="{'active':activeLink=='#about'}" href="#about"><i class="bi bi-file-earmark-person"></i>About</a>
                 <a class="nav-link" @click.stop="closeSidebar" :class="{'active':activeLink=='#contact'}" href="#contact"><i class="bi bi-phone"></i>Contact</a>
                 <a class="nav-link" @click.stop="closeSidebar" :class="{'active':activeLink=='#portfolio'}" href="#portfolio"><i class="bi bi-collection"></i>Portfolio</a>
-                <a class="nav-link" @click.stop="closeSidebar" href="./Javier Bryan- Resume 2021.pdf" target="_blank"><i class="bi bi-terminal"></i>Resume</a>
+                <a class="nav-link" @click.stop="closeSidebar" href="./Javier Bryan- Resume 2022.pdf" target="_blank"><i class="bi bi-terminal"></i>Resume</a>
                 <p class="sidebar__footer">Javier Bryan | 2021</p>
             </div>
         </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -40,7 +40,7 @@
                 most creative and imaginative thoughts into reality as I view knowledge as an omnipotent tool when placed in the right hands.
                 I try my best to learn something new everday as I do not believe it's possible to learn it all at once.
               </p>
-              <a class="text-white" href="./Javier Bryan- Resume 2021.pdf" target="_blank">Check out my resume</a>
+              <a class="text-white" href="./Javier Bryan- Resume 2022.pdf" target="_blank">Check out my resume</a>
               <p class="mt-3">Contact me here!</p>
               <div class="contacts">
                 <p><span>Email <i class="bi bi-mailbox"></i></span> : <a class="text-white" href="mailto: javierbryan11@gmail.com">javierbryan11@gmail.com</a> </p>
