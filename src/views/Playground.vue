6be995efac3f4225581d5365d0e4040168ac6df3
Javier <javierbryan11@gmail.com>
2022-05-26 21:08:50 -0500

    added welcome text and swinging doors

--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -5,15 +5,18 @@
 <script lang="ts">
 
 import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, CylinderGeometry,
-  MeshStandardMaterial, PointLight, PointLightHelper, Mesh, 
+  MeshStandardMaterial, PointLight, PointLightHelper, Mesh,
   MathUtils, AmbientLight, DoubleSide, Object3D, TextureLoader,
-  PlaneGeometry, DirectionalLight, Vector3 } from 'three';
+  DirectionalLight, Vector3, CircleGeometry } from 'three';
 
 const profileImage = require('../assets/profile.jpg');
 const sceneBackground = require("../assets/scenebackground.jpg");
 
 const hingeTexture = require('../assets/silvertexture.jpg');
 const hingeNormalTexture = require("../assets/silvernormaltexture.jpg");
+
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 
 import Vue from 'vue'
 export default Vue.extend({
@@ -30,16 +33,17 @@ export default Vue.extend({
 
     const renderer =  new WebGLRenderer({ alpha: true});
     const textureLoader = new TextureLoader();
+    const fontLoader = new FontLoader();
     
-    const pointLight = new PointLight(0xdddddd);
-    const pointLightHelper = new PointLightHelper(pointLight);
+    const topDirectionLight = new DirectionalLight(0xdddddd, 0.9);
+    // const pointLightHelper = new PointLightHelper(pointLight);
 
     const ambientLight = new AmbientLight(0x404040);
-    const directionalLight =  new DirectionalLight(0xffffff, 0.9);
+    const groundDirectionalLight =  new DirectionalLight(0xffffff, 0.9);
 
     const scrollDirection = 0;
 
-    const journeyPlaneGeometry = new PlaneGeometry(10000,10000);
+    const journeyPlaneGeometry = new CircleGeometry(10000,10000);
     const journeyPlaneMaterial = new MeshStandardMaterial({ 
       side: DoubleSide,
       color: 0xffffff
@@ -66,6 +70,10 @@ export default Vue.extend({
     const rightDoorHinge = new Mesh(rightDoorHingeGeometry, rightDoorHingeMaterial);
     const rightDoorHingePivot =  new Object3D();
 
+    // -1 - Forward and  1 - Backword
+    const rightDoorDirection = -1; 
+    const leftDoorDirection = -1;
+
     const plaqueGeometry = new BoxGeometry(modelWidth, 1, modelHeight );
     const plaqueMaterial = new MeshStandardMaterial({
       side: DoubleSide,
@@ -81,20 +89,20 @@ export default Vue.extend({
       map: textureLoader.load(profileImage)
     });
 
-    const projectOne = new Mesh(projectOneGeometry, projectOneMaterial);
-
+    const projectOne = new Mesh(projectOneGeometry, projectOneMaterial); 
+    
     return {
       scene,
       camera,
       renderer,
-      pointLight,
+      topDirectionLight,
       ambientLight,
       scrollDirection,
       textureLoader,
       journeyPlane, 
       plaque,
       projectOne,
-      directionalLight,
+      groundDirectionalLight,
       cameraDirection, 
       leftDoorHinge,
       rightDoorHinge,
@@ -103,7 +111,10 @@ export default Vue.extend({
       hingeRadius,
       modelGap,
       leftDoorHingePivot,
-      rightDoorHingePivot
+      rightDoorHingePivot,
+      rightDoorDirection,
+      leftDoorDirection,
+      fontLoader
     }
   },
   created(){
@@ -115,40 +126,56 @@ export default Vue.extend({
     // add point light helper to scene
     // this.scene.add(this.pointLightHelper);
 
-    this.scene.add(this.pointLight);
+    this.scene.add(this.topDirectionLight);
 
     // add ambient light to scene
     // this.scene.add(this.ambientLight);
 
     // set point light and camera initial position
-    this.pointLight.position.y = 1000;
+    this.topDirectionLight.position.y = 1000;
     this.camera.position.z = 500;
-    this.camera.position.y = 100;
+    this.camera.position.y = 200;
 
     this.scene.add(this.journeyPlane);
     this.journeyPlane.rotateX( MathUtils.degToRad(90));
 
-    this.scene.add( this.leftDoorHinge, this.rightDoorHinge);
+    this.scene.add(this.leftDoorHinge, this.rightDoorHinge);
     
     this.leftDoorHinge.position.y = this.modelHeight / 2;
-    this.leftDoorHinge.position.x = this.modelWidth + this.hingeRadius + this.modelGap;
+    this.leftDoorHinge.position.x = -(this.modelWidth + this.hingeRadius + this.modelGap);
 
     this.rightDoorHinge.position.y = this.modelHeight / 2;
-    this.rightDoorHinge.position.x = -(this.modelWidth + this.hingeRadius + this.modelGap);
+    this.rightDoorHinge.position.x = this.modelWidth + this.hingeRadius + this.modelGap;
 
 
     this.projectOne.rotateX(MathUtils.degToRad(-90));
-    this.projectOne.position.x = -this.modelWidth / 2;
+    this.projectOne.position.x = this.modelWidth / 2;
     this.leftDoorHingePivot.add(this.projectOne);
     this.leftDoorHinge.add(this.leftDoorHingePivot);
 
     this.plaque.rotateX(MathUtils.degToRad(-90));
-    this.plaque.position.x = this.modelWidth / 2;
+    this.plaque.position.x = -this.modelWidth / 2;
     this.rightDoorHingePivot.add(this.plaque);
     this.rightDoorHinge.add(this.rightDoorHingePivot);
 
-    this.directionalLight.position = new Vector3(0, 0, 1);
-    this.scene.add(this.directionalLight);
+    this.groundDirectionalLight.position = new Vector3(0, 0, 1);
+    this.scene.add(this.groundDirectionalLight);
+
+
+    this.fontLoader.load("/fonts/Droid_Sans_Regular.json", (font)=>{
+      const welcomeMessageGeometry = new TextGeometry("Welcome To My Playground", {
+        font,
+        size: 50,
+        height: 50,
+      });
+      const welcomeMessageMaterial = new MeshStandardMaterial({ color: 0xdddddd });
+      this.welcomeMessage = new Mesh(welcomeMessageGeometry, welcomeMessageMaterial);
+      
+      this.welcomeMessage.position.x = -400;
+      this.welcomeMessage.position.y = 350;
+      this.welcomeMessage.position.z = 100;
+      this.scene.add(this.welcomeMessage);
+    });
   },
   mounted(){
     document.getElementById("canvas").appendChild(this.renderer.domElement);
@@ -188,8 +215,23 @@ export default Vue.extend({
       document.removeEventListener("mousemove", this.turnCamera);
     },
     oscillateHinge(){
-      this.leftDoorHingePivot.rotation.y -= 0.01;
-      this.rightDoorHingePivot.rotation.y += 0.01;
+      const oscillationSpeed = 0.02;
+
+      if(this.leftDoorDirection < 0 && this.leftDoorHingePivot.rotation.y < -(Math.PI / 6)){
+        this.leftDoorDirection = 1;
+      }
+      else if (this.leftDoorDirection > 0 && this.leftDoorHingePivot.rotation.y > (Math.PI / 6)){
+        this.leftDoorDirection = -1;
+      }
+
+      if(this.rightDoorDirection < 0 && this.rightDoorHingePivot.rotation.y > (Math.PI / 6)){
+        this.rightDoorDirection = 1;
+      }
+      else if (this.rightDoorDirection > 0 && this.rightDoorHingePivot.rotation.y < -(Math.PI / 6)){
+        this.rightDoorDirection = -1;
+      }
+      this.leftDoorHingePivot.rotation.y += this.leftDoorDirection * oscillationSpeed;
+      this.rightDoorHingePivot.rotation.y -= this.rightDoorDirection * oscillationSpeed;
     }
   }
 })
@@ -203,5 +245,21 @@ export default Vue.extend({
   left: 0;
   width: 100vw;
   height: 100vh;
+}
+
+#canvas::after {
+  content:"My playground is currently a work in progress. Follow along with me on this construction journey by staying up to date 🚧👷‍♂️";
+  position: absolute;
+  bottom: 1rem;
+  right: 1rem;
+  max-width: 20rem;
+  padding: 1rem;
+  font-size: 0.7rem;
+  text-align: left;
+  color: var(--tertiary-color);
+  font-weight: bold;
+  backdrop-filter: blur(8px);
+  background-color: #ddd;
+  border-radius: 8px;
 }
 </style>
